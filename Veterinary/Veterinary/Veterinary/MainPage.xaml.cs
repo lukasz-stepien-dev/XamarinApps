0303a41a9efaa32c17da1fdbefd22b49de35f50e
Łukasz Stępień <lukaszatepien@gmail.com>
2024-02-21 13:58:18 +0100

    21-02-24

---
+++ Veterinary/Veterinary/Veterinary/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Veterinary
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            var species = new List<string>
+            {
+                "Pies",
+                "Kot",
+                "Świnka morska",
+            };
+            Species.ItemsSource = species;
+        }
+
+        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            AgeLabel.Text = ((int)e.NewValue).ToString();
+        }
+
+        private void Species_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            Console.WriteLine(e.Item.ToString());
+            switch (e.Item.ToString())
+            {
+                case "Pies":
+                    AgeSlider.Maximum = 18;
+                    break; 
+                case "Kot":
+                    AgeSlider.Maximum = 20;
+                    break;
+                case "Świnka morska":
+                    AgeSlider.Maximum = 9;
+                    break;
+            }
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            string message = OwnerInp.Text + ", " + Species.SelectedItem + ", " + PurposeInp.Text + ", " + TimePicker.Time;
+            DisplayAlert("Zapisana Wizytę", message, "OK");
+        }
+    }
+}
